345864e0f064e1a8bc9dbbad37af6a245985c300
Syed Shahid <Daniyalshahid1221@gmail.com>
2025-09-07 16:55:14 -0400

    509. Fibonacci Number

---
+++ Easy/509. Fibonacci Number.py
@@ -0,0 +1,22 @@
+#https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 1:
+            return n
+        if n == 0:
+            return n
+        return self.fib(n-2) + self.fib(n-1)
+
+s = Solution()
+print(s.fib(4))
+"""
+Notes/Realizations
+ - In all languages except JavaScript and the idea of hoisting (declarations of variables and functions are moved to the top of the defined scope), function declarations must come BEFORE function call.
+ - Fibonacci Sequence only applies to the TWO preceding numbers, not a sum of ALL numbers. Can't believe I didn't notice this...
+ - When two or more recursive calls are made in the return statemnt, the calls must be terminated fully from left to right before moving onto the next one.
+    :Stack frames are made for each recursive call of fib(n-2) until we get to fib(1) + fib(0), then all stack frames are popped and the resulting value is finalized in fib(n-2)
+ 
+ FYI
+ Objects are instances of classes, so in order to use the defined Solution, create an object of Solution then call it's internal function
+"""
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SDS-Leetcode-Solutions
 I'm gonna save leetcode solutions here whenever I do them. If you're a recruiter please hire me I will code for money. 
+
+Will have notes and solutions!!!
